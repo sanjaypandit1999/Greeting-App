3ce52a45d8115379497b654c4e04a9bd8fb2f352
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-07 12:16:19 +0530

    MODIFIED: POST request method add message to in database

--- GreetingApp/src/main/java/com/bridgelabz/greeting/controller/GreetingController.java
+++ GreetingApp/src/main/java/com/bridgelabz/greeting/controller/GreetingController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,13 +29,13 @@ public class GreetingController {
 		return greetingService.getHelloMessage();
 	}
 
-	@GetMapping("/customMessage")
+	@PostMapping("/customMessage")
 	public String getCustomMessage(@RequestBody UserDto userDto) {
 		return greetingService.getCustomMessage(userDto);
 	}
 
-	@GetMapping("/find_greeting")
-	public User findGreetById(@RequestParam int id) {
+	@GetMapping("/find_greeting/{id}")
+	public User findGreetById(@PathVariable int id) {
 		return greetingService.getGreetById(id);
 	}
 
@@ -42,8 +44,8 @@ public class GreetingController {
 		return greetingService.getAllMessage();
 	}
 
-	@DeleteMapping("/delete_greet")
-	public String deleteGreet(@RequestParam int id) {
+	@DeleteMapping("/delete_greet/{id}")
+	public String deleteGreet(@PathVariable int id) {
 		return greetingService.deleteGreet(id);
 	}
 }
